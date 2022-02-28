0abf52890b036e1d538ce77e748ad612d460e86b
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-02-28 13:01:54 -0800

    create the custom alarmtableviewcell

---
+++ AlarmApp/Controllers/Custom Cells/AlarmTableViewCell.swift
@@ -0,0 +1,60 @@
+//
+//  AlarmTableViewCell.swift
+//  AlarmApp
+//
+//  Created by Mehdi MMS on 28/02/2022.
+//
+
+import UIKit
+
+/**
+The protocol we will use to handle the updating of the cell when the `isEnabledButton` is toggled
+   - `class`: This protocol can interact with class level objects
+
+Step One:
+   - Define the protocol. This will need to interact with class level objects.
+
+Delegate Methods:
+   - alarmWasToggled(sender: AlarmTableViewCell)
+
+*/
+protocol AlarmTableViewCellDelegate: class{
+    /**
+     Delegate method for the `AlarmTableViewCellDelegate`
+     
+     Parameters:
+     - sender: The cell that that user toggled
+     */
+    func alarmWasToggled(sender: AlarmTableViewCell)
+}
+
+
+class AlarmTableViewCell: UITableViewCell {
+
+    //MARK: - Outlets
+    @IBOutlet weak var alarmTitleLabel: UILabel!
+    @IBOutlet weak var alarmFireDateLabel: UILabel!
+    @IBOutlet weak var isEnabledSwitch: UISwitch!
+
+    /**
+     The `delegate` or *intern* for the protocol `AlarmTableViewCellDelegate`
+
+     - weak: We mark this method as weak to not create a retain cycle
+     - optional: We do not want to set the value of the delegate here.
+     */
+    weak var delegate: AlarmTableViewCellDelegate?
+
+    //MARK: - Helper Functions
+   func updateViews(with alarm: Alarm) {
+        alarmTitleLabel.text = alarm.title
+    /// We can be confident with `Force Unwrapping` the `fireDate` because a alarm can not be created without one.
+        alarmFireDateLabel.text = alarm.fireDate!.stringValue()
+        isEnabledSwitch.isOn = alarm.isEnabled
+    }
+
+    //MARK: - Actions
+    @IBAction func isEnabledSwitchToggled(_ sender: Any) {
+        /// This is the call to action for the delegate. Hey intern, go get me a coffee
+        delegate?.alarmWasToggled(sender: self)
+    }
+}
